0283b598eedf12b1931b39ccd0a52b2813ba1c4a
wbrunovieira <wbrunovieira77@gmail.com>
2020-09-15 10:34:55 +0100

    practise Unit test

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            // reverse the word // hello // olleh
+
+
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            //compare the origin  with the reverse
+            if (reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            // return wheather they are the same or not
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void MyTest(string word, bool expected)
         {
+            //arrange
 
-        }
+            var tester = new WordSmith();
+
+            // act
+
+            var actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(actual, expected);
+        } 
     }
 }
